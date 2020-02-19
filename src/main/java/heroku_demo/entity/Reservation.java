58a7a27a18957771d52c2dcef2e84d64a51f80ec
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-02-19 21:09:56 +0100

    Added empty constructor to the Reservation class

--- src/main/java/heroku_demo/entity/Reservation.java
+++ src/main/java/heroku_demo/entity/Reservation.java
@@ -18,6 +18,11 @@ public class Reservation {
     private String bed;
     private Time time;
 
+    public Reservation() {
+        name="";
+        email="";
+    }
+
     public Reservation(String name, String email) {
         this.name = name;
         this.email = email;
